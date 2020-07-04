a022375b6325faa4ab6341570d1305a583c4bc0d
RiverTwilight <wrj2014@126.com>
2020-07-04 19:53:37 +0800

    fix bugs

--- src/layout/drawer.js
+++ src/layout/drawer.js
@@ -3,7 +3,7 @@ import {
   NavLink
 } from "react-router-dom"
 import { Drawer } from 'mdui'
-import applist from '../utils/appList'
+import applist from '../utils/appList.js'
 import { getUserInfo } from '../utils/UserInfo'
 
 const list = [{
