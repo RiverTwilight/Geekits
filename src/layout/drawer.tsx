19e104f2dcbe1634b5b94cc342a0426f545f451f
root <root@DESKTOP-LKA6U3R.localdomain>
2020-08-26 14:28:49 +0800

    [ts-migrate][layout] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- src/layout/drawer.tsx
+++ src/layout/drawer.tsx
@@ -3,7 +3,9 @@ import {
     NavLink
 } from "react-router-dom"
 import { Drawer } from 'mdui'
+// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module '../u... Remove this comment to see the full error message
 import { getUserInfo } from '../utils/Services/UserInfo'
+// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module '../u... Remove this comment to see the full error message
 import applist from '../utils/applist'
 
 const list = [{
@@ -19,16 +21,21 @@ const list = [{
 }]
 
 const Menu = () => (
+    // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <>{
         list.map((a) => (
+            // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
             <NavLink
                 onClick={() => {
+                    // @ts-expect-error ts-migrate(2339) FIXME: Property 'leftDrawer' does not exist on type 'Wind... Remove this comment to see the full error message
                     window.innerWidth <= 1024 && window.leftDrawer.close()
                 }}
                 key={a.link}
                 exact className="mdui-list-item mdui-ripple"
                 activeClassName="mdui-list-item-active" to={a.link}>
+                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                 <i className={"mdui-list-item-icon mdui-icon material-icons mdui-text-color-" + a.iconColor}>{a.icon}</i>
+                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                 <div className="mdui-list-item-content">{a.text}</div>
             </NavLink>
         ))
@@ -37,34 +44,48 @@ const Menu = () => (
 
 class DrawerMenu extends React.Component<{}, {}> {
     componentDidMount() {
+        // @ts-expect-error ts-migrate(2339) FIXME: Property 'leftDrawer' does not exist on type 'Wind... Remove this comment to see the full error message
         window.leftDrawer = new Drawer('#left-drawer');
     }
     render() {
         const user = getUserInfo();
         return (
+            // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
             <div id="left-drawer" className="mdui-drawer">
+                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                 <div style={{ height: '130px' }} className="mdui-shadow-0 mdui-card">
+                    {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                     <div className="mdui-card-primary">
+                        {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                         <div className="mdui-card-primary-title mdui-text-color-theme">云极客工具</div>
-                        <div className="mdui-card-primary-subtitle">{`共有${applist.filter(app => app.channel !== 5).length}个工具`}</div>
+                        {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
+                        <div className="mdui-card-primary-subtitle">{`共有${applist.filter((app: any) => app.channel !== 5).length}个工具`}</div>
                     </div>
                 </div>
+                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                 <ul className="mdui-list">
+                    {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                     <NavLink
                         onClick={() => {
+                            // @ts-expect-error ts-migrate(2339) FIXME: Property 'leftDrawer' does not exist on type 'Wind... Remove this comment to see the full error message
                             window.innerWidth <= 1024 && window.leftDrawer.close()
                         }}
                         exact className="mdui-list-item mdui-ripple"
                         activeClassName="mdui-list-item-active" to={user ? '/user' : '/user/login'}>
+                        {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                         <i className="mdui-list-item-avatar mdui-icon material-icons">face</i>
+                        {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                         <div className="mdui-list-item-content">{user ? user.username : '未登录'}</div>
                     </NavLink>
+                    {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                     <div className="mdui-divider" />
+                    {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                     <Menu />
                 </ul>
+                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                 <p className="mdui-text-center copyright">©2019-2020&nbsp;江村暮</p>
             </div>
-        )
+        );
     }
 }
 
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+// @ts-expect-error ts-migrate(2305) FIXME: Module '"mdui"' has no exported member 'confirm'.
 import { confirm, snackbar } from 'mdui'
+// @ts-expect-error ts-migrate(6142) FIXME: Module './drawer' was resolved to '/mnt/h/Bob/Web/... Remove this comment to see the full error message
 import Drawer from './drawer'
 
 //将一言添加到便签
-const addSaying2Fiv = saying => {
+const addSaying2Fiv = (saying: any) => {
     var content = `  <br>${saying.say}  ———来自 ${saying.from}`
     var today = new Date()
     var newNote = {
@@ -15,7 +17,7 @@ const addSaying2Fiv = saying => {
     if (localStorage.note) {
         var notes = JSON.parse(localStorage.note);
         var hasNote = false;
-        notes.map(note => {
+        notes.map((note: any) => {
             if (note.title === '一言收藏') hasNote = true
         })
         if (hasNote) {
@@ -35,8 +37,10 @@ const addSaying2Fiv = saying => {
     localStorage.setItem('note', JSON.stringify(notes))
 }
 
-export default class extends React.Component {
-    constructor(props) {
+type State = any;
+
+export default class extends React.Component<{}, State> {
+    constructor(props: {}) {
         super(props);
         this.state = {
             searchResult: '',
@@ -64,25 +68,37 @@ export default class extends React.Component {
     }
     componentDidMount() {
         this.loadSaying()
+        // @ts-expect-error ts-migrate(2339) FIXME: Property 'getRef' does not exist on type 'Readonly... Remove this comment to see the full error message
         this.props.getRef([
+            // @ts-expect-error ts-migrate(2339) FIXME: Property 'headerTitle' does not exist on type 'def... Remove this comment to see the full error message
             { name: 'title', ref: this.headerTitle },
+            // @ts-expect-error ts-migrate(2339) FIXME: Property 'menuBtn' does not exist on type 'default... Remove this comment to see the full error message
             { name: 'menuBtn', ref: this.menuBtn }
         ]);// 将ref传给父组件   
     }
     render() {
         const { saying } = this.state
         return (
+            // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
             <>
+                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                 <Drawer />
+                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                 <header
                     className={`mdui-shadow-0 mdui-appbar mdui-appbar-fixed`}>
+                    {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                     <div className="mdui-appbar mdui-shadow-0">
+                        {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                         <div className="mdui-toolbar mdui-color-white">
+                            {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                             <button
+                                // @ts-expect-error ts-migrate(2339) FIXME: Property 'leftDrawer' does not exist on type 'Wind... Remove this comment to see the full error message
                                 onClick={() => window.leftDrawer.toggle()}
                                 className="mdui-btn mdui-btn-icon mdui-text-color-theme">
+                                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                                 <i className="mdui-icon material-icons">menu</i>
                             </button>
+                            {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                             <a
                                 onClick={() => {
                                     confirm(
@@ -108,26 +124,36 @@ export default class extends React.Component {
                                     )
                                 }}
                             >
+                                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                                 <div
+                                    // @ts-expect-error ts-migrate(2339) FIXME: Property 'headerTitle' does not exist on type 'def... Remove this comment to see the full error message
                                     ref={r => this.headerTitle = r}
                                     className="mdui-typo-title header-width-saying"
                                 >
+                                    {/* @ts-expect-error ts-migrate(2339) FIXME: Property 'title' does not exist on type 'Readonly<... Remove this comment to see the full error message */}
                                     {this.props.title || '云极客工具'}
                                 </div>
+                                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                                 <span className="mdui-typo-caption-opacity mdui-text-truncate saying">{saying.say}</span>
                             </a>
+                            {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                             <div className="mdui-toolbar-spacer"></div>
+                            {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                             <button
                                 style={{ display: 'none' }}
+                                // @ts-expect-error ts-migrate(2339) FIXME: Property 'menuBtn' does not exist on type 'default... Remove this comment to see the full error message
                                 ref={r => this.menuBtn = r}
                                 onClick={() => {
+                                    // @ts-expect-error ts-migrate(2339) FIXME: Property 'menu' does not exist on type 'Window & t... Remove this comment to see the full error message
                                     window.menu && window.menu()
                                 }}
                                 className="mdui-hidden-sm-up mdui-btn mdui-btn-icon mdui-text-color-theme">
+                                {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                                 <i className="mdui-icon material-icons">more_vert</i>
                             </button>
                         </div>
                     </div>
+                    {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
                     <div className="mdui-divider"></div>
                 </header>
             </>
