0a67ab1b0296cb1c2354ffedf3be8d6db2e4d53a
RiverTwilight <wrj2014@126.com>
2020-09-06 13:57:53 +0800

    <fix>global api

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    //baseURL: 'https://api.ygktool.cn',
-    baseURL: 'http://localhost:444',
+    baseURL: 'https://api.ygktool.cn',
+    // baseURL: 'http://localhost:444',
     timeout: 15000
 })
