f698413c0ac7511a794901cd45ea62c6c26c3b04
RiverTwilight <wrj2014@126.com>
2020-10-18 11:59:46 +0800

    <feat>九格切图

--- src/apps/policy_generator/generator.ts
+++ src/apps/policy_generator/generator.ts
@@ -1,15 +1,16 @@
 export default class PG {
-    constructor(){
-
-    }
-    setFirm(){
-        
-    }
-    generator(){
-        return `## 用户协议
-
-        本协议是您与${23}（下称“我们”）之间关于${23}（下称“本网站”）的法律协议。
-        
-        在使用本网站时，用户必须要遵从国家或地区的相关法律。`
-    }
+	config: {
+		[profile: string]: string;
+	};
+	constructor() {
+		this.config = {};
+	}
+	setConfig() {}
+	generator(): string {
+		return `
+本协议是您与${
+			this.config.firmName
+		}（下称“我们”）之间关于${23}（下称“本网站”）的法律协议。
+在使用本网站时，用户必须要遵从国家或地区的相关法律。`;
+	}
 }
--- src/apps/policy_generator/index.tsx
+++ src/apps/policy_generator/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Input, Button } from "mdui-in-react";
 import useInput from "../../utils/Hooks/useInput";
 import PG from "./generator";
+import marked from "marked";
 // TODO 隐私政策生成器
 
 const PGI = new PG();
@@ -9,6 +10,8 @@ const PGI = new PG();
 const BasicInfo = ({ display }: { display: boolean }) => {
 	const [firmName, setFirmName] = useInput("");
 	const [shortName, setShortName] = useInput("");
+	const [websiteName, setWebsiteName] = useInput("");
+	const [url, setUrl] = useInput("");
 	return (
 		<div
 			style={{
@@ -17,12 +20,90 @@ const BasicInfo = ({ display }: { display: boolean }) => {
 		>
 			<Input header="公司名称" onInput={setFirmName} value={firmName} />
 			<Input header="公司简称" onInput={setShortName} value={shortName} />
+			<Input
+				header="网站名称"
+				onInput={setWebsiteName}
+				value={websiteName}
+			/>
+			<Input header="网站地址" onInput={setUrl} value={url} />
+		</div>
+	);
+};
+
+const AdditionalInfo = ({ display }: { display: boolean }) => {
+	const [firmName, setFirmName] = useInput("");
+	const [shortName, setShortName] = useInput("");
+	const [websiteName, setWebsiteName] = useInput("");
+	const [url, setUrl] = useInput("");
+	return (
+		<div
+			style={{
+				display: display ? "block" : "none",
+			}}
+		>
+			<Input header="公司名称" onInput={setFirmName} value={firmName} />
+			<Input header="公司简称" onInput={setShortName} value={shortName} />
+			<Input
+				header="网站名称"
+				onInput={setWebsiteName}
+				value={websiteName}
+			/>
+			<Input header="网站地址" onInput={setUrl} value={url} />
+		</div>
+	);
+};
+
+const Result = ({ display }: { display: boolean }) => {
+	return (
+		<div
+			style={{
+				display: display ? "block" : "none",
+			}}
+		>
+			<h3>完毕</h3>
+			<div
+				dangerouslySetInnerHTML={{
+					__html: marked(PGI.generator()),
+				}}
+			></div>
 		</div>
 	);
 };
 
 const PolicyType = ({ display }: { display: boolean }) => {
 	const [type, setType] = React.useState(0);
+	const TypeCrad = ({
+		title,
+		description,
+		index,
+	}: {
+		title: string;
+		description: string;
+		index: number;
+	}) => (
+		<React.Fragment>
+			<div
+				key={title}
+				className={`${
+					type === index && "mdui-shadow-12"
+				} mdui-card mdui-hoverable`}
+				onClick={() => {
+					setType(index);
+				}}
+			>
+				<div className="mdui-card-primary">
+					<div className="mdui-card-primary-title">
+						{type === index && "✔"}
+						{title}
+					</div>
+					<div className="mdui-card-primary-subtitle">
+						{description}
+					</div>
+				</div>
+			</div>
+			<br></br>
+		</React.Fragment>
+	);
 	return (
 		<div
 			style={{
@@ -31,19 +112,22 @@ const PolicyType = ({ display }: { display: boolean }) => {
 		>
 			<h3>你要生成的类型</h3>
 			<p>欢迎使用服务协议生成器</p>
-			<div className="mdui-card mdui-hoverable">
-				<div className="mdui-card-primary">
-					<div className="mdui-card-primary-title">Title</div>
-					<div className="mdui-card-primary-subtitle">Subtitle</div>
-				</div>
-			</div>
+			{[
+				{
+					title: "隐私协议",
+					description:
+						"我们的隐私政策生成器可以帮助您确保您的业务符合法律规定。我们在这里帮助您保护您的业务，您自己和您的客户",
+				},
+			].map((type, i) => (
+				<TypeCrad {...type} index={i} />
+			))}
 		</div>
 	);
 };
 
 export default () => {
 	const [step, setStep] = React.useState(0);
-	const TOTAL_STEP = 10;
+	const TOTAL_STEP = 3;
 	const nextStep = () => {
 		setStep(step + 1);
 	};
@@ -56,7 +140,7 @@ export default () => {
 				<div
 					className="mdui-progress-determinate"
 					style={{
-						width: `${(step / 10) * 100}%`,
+						width: `${(step / TOTAL_STEP) * 100}%`,
 					}}
 				></div>
 			</div>
@@ -64,6 +148,7 @@ export default () => {
 				<PolicyType display={step === 0} />
 				<BasicInfo display={step === 1} />
 				<BasicInfo display={step === 2} />
+				<Result display={step === 3} />
 			</div>
 
 			<br></br>
