263dbbad61345e103c2e09728bbf49e09767db0f
RiverTwilight <wrj2014@126.com>
2021-06-05 14:32:00 +0800

    add todo

--- src/index.js
+++ src/index.js
@@ -56,4 +56,5 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// TODO web-vital
 reportWebVitals(console.log);
