b376c2b8996fbaeb32a497c04533b3d23edc6262
RiverTwilight <wrj2014@126.com>
2021-06-17 14:31:38 +0000

    feat: 新工具

--- src/apps/point_brochure/index.tsx
+++ src/apps/point_brochure/index.tsx
@@ -12,19 +12,21 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import Dialog from "@material-ui/core/Dialog";
 import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List"
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 
 const DEFAULT_DATA = {
-    tasks:[
+    tasks: [
 
     ],
-    brochures:[
+    brochures: [
         {
             name: "",
-            reward:[
+            reward: [
                 {
                     points: 100,
                     gift: "礼物"
@@ -84,15 +86,51 @@ const RewardBoard = () => {
 };
 
 const TaskList = () => {
-    return null;
+    return (
+        <Card>
+
+        </Card>
+    );
 };
 
 // TODO 积分手册工具
 const App = () => {
+    let transaction: any = null;
+    React.useEffect(() => {
+        let openRequest = indexedDB.open("YGK_BROCHURE");
+        openRequest.onupgradeneeded = function () {
+            let db = openRequest.result;
+
+            if (!db.objectStoreNames.contains('tasks')) { // if there's no "books" store
+                db.createObjectStore('tasks', { keyPath: 'id' }); // create it
+            }
+
+        };
+
+        openRequest.onerror = function () {
+            console.error("Error", openRequest.error);
+        };
+
+        openRequest.onsuccess = function () {
+            let db = openRequest.result;
+
+            // continue working with database using db object
+        };
+    }, [])
     return (
         <>
             <RewardBoard />
             <TaskList />
+            <Button onClick={() => {
+                let books = transaction && transaction.objectStore("tasks");
+                let book = {
+                    id: 'js',
+                    price: 10,
+                    created: new Date()
+                  };
+                  
+                  let request = books.add(book); // (3)
+            }} >fd</Button>
         </>
     );
 };
