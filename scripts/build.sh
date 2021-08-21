ebaddf6c270f6e75c3174e1f5d1efe9a3222d5c0
RiverTwilight <wrj2014@126.com>
2021-08-21 17:05:59 +0800

    [build] add vercel config

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,12 @@
-react-scripts build && cp public/service-worker.js build/
+#!/bin/bash
+# Since the v2 branch use different framework with v1, we need to create 2 projects to deploy them.
+if [[ "$ENABLE_BUILD_V2" == "true" ]]; then
+    # Proceed with the build
+    echo "✅ - Build can proceed";
+    exit 1;
+
+else
+    # Don't build
+    echo "🛑 - Build cancelled";
+    exit 0;
+fi
