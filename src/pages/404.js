28e1152779f9da4d9212faa90ecdfa5b7b6afde9
RiverTwilight <wrj2014@126.com>
2022-11-02 12:31:23 +0800

    feat: use next image component for 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 export default function Custom404() {
 	return (
 		<div className="center-with-flex">
-			<img
-				alt="404"
+			<Image
+				alt="Illustration of a 404 page"
 				height="200"
 				width="200"
 				src="/illustration/undraw_taken_re_yn20.svg"
-			></img>
+			/>
 			<Typography variant="h4">电波无法到达哦</Typography>
 			<Typography variant="body1">
 				想要的工具不见了？返回首页找找吧！
