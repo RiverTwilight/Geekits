efd710b76c988dbcb3260a21ce6bd86634fd352e
RiverTwilight <wrj2014@126.com>
2022-11-28 19:20:44 +0800

    test: appconfig test

--- src/apps/__development/index.tsx
+++ src/apps/__development/index.tsx
@@ -1,24 +1,6 @@
 import React from "react";
-import html2canvas from "html2canvas";
-import saveFile from "../../utils/fileSaver";
-import Button from "@mui/material/Button";
-import Slider from "@mui/material/Slider";
-import SliderWithIcon from "../../components/SliderWithIcon";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
-import ListItemText from "@mui/material/ListItemText";
-import Switch from "@mui/material/Switch";
-import BorderVerticalIcon from "@mui/icons-material/BorderVertical";
-import ColorLensIcon from "@mui/icons-material/ColorLens";
-import OutlinedCard from "../../components/OutlinedCard";
 
-const IfBr = ({ statu }: any) => {
-	return statu === "vertical" ? <br></br> : null;
-};
-
-const FakeLogo = ({ hStyle, frontStyle, lastStyle }: any) => {
+const Development = ({ hStyle, frontStyle, lastStyle }: any) => {
 	return (
 		<div
 			style={{
@@ -33,98 +15,8 @@ const FakeLogo = ({ hStyle, frontStyle, lastStyle }: any) => {
 				alignItems: "center",
 			}}
 			id="blackborad"
-		>
-			<h1
-				style={{
-					marginTop: "20px",
-					fontFamily: "SimHei,STHeiti",
-					fontWeight: 1000,
-					letterSpacing: "-1.5px",
-					fontSize: hStyle.size + "em",
-				}}
-				className="mdui-text-center"
-			>
-				<span
-					style={{
-						borderRadius: "4px",
-						color: frontStyle.color,
-						backgroundColor: frontStyle.backgroundColor,
-					}}
-					onInput={(e) => {
-						/*props.onTextChange({
-                        front:{
-                            text:e.target.innerText,
-                            backgroundColor:props.frontStyle.backgroundColor
-                        }
-                    })*/
-					}}
-					contentEditable={true}
-				>
-					Ygkt
-				</span>
-				<IfBr statu={hStyle.array} />
-				<span
-					style={{
-						display: "inline",
-						backgroundColor: lastStyle.backgroundColor,
-						borderRadius: "4px",
-						color: lastStyle.color,
-						padding: "0px 4px 0px 4px",
-						marginLeft: "3px",
-					}}
-					contentEditable={true}
-				>
-					ool
-				</span>
-			</h1>
-		</div>
+		></div>
 	);
 };
 
-type UiState = any;
-
-export default class FakePornhubLogo extends React.Component<{}, UiState> {
-	inputRef: any;
-	constructor(props: {}) {
-		super(props);
-		this.state = {
-			hStyle: {
-				size: 4.0,
-				array: "transverse",
-			},
-			front: {
-				color: "#ffffff",
-				backgroundColor: "transparent",
-			},
-			last: {
-				color: "#000000",
-				backgroundColor: "#f79817",
-			},
-		};
-	}
-	handleDownload = () => {
-		html2canvas(document.querySelector("#blackborad")).then((canvas) => {
-			let base64 = canvas.toDataURL("image/png");
-			saveFile({
-				file: base64,
-				type: "png",
-				filename: "ygktool-fake_pornhub_logo.png",
-			});
-		});
-	};
-	render() {
-		const { hStyle, front, last } = this.state;
-		return (
-			<>
-				<div className="center-with-flex">
-					<FakeLogo
-						hStyle={hStyle}
-						frontStyle={front}
-						lastStyle={last}
-					/>
-				</div>
-				<br />
-			</>
-		);
-	}
-}
+export default Development;
