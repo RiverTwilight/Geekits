c9e1eca5f6be1286c434837d46babc3e282d8bf4
RiverTwilight <wrj2014@126.com>
2022-11-28 20:07:31 +0800

    fix: build error

--- src/apps/__development/index.tsx
+++ src/apps/__development/index.tsx
@@ -1,22 +1,7 @@
 import React from "react";
 
-const Development = ({ hStyle, frontStyle, lastStyle }: any) => {
-	return (
-		<div
-			style={{
-				paddingTop: "50px",
-				borderRadius: "4px",
-				backgroundColor: "#000000",
-				width: "90%",
-				maxWidth: "600px",
-				height: "250px",
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-			id="blackborad"
-		></div>
-	);
+const Development = (props) => {
+	return <div></div>;
 };
 
 export default Development;
