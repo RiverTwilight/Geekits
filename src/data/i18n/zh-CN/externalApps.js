3002981fe1a29bb90b520b181d7143fcc4b9ff19
RiverTwilight <wrj2014@126.com>
2022-12-27 21:58:00 +0800

    other: update external app

--- src/data/i18n/zh-CN/externalApps.js
+++ src/data/i18n/zh-CN/externalApps.js
@@ -22,7 +22,7 @@ const data = [
 		id: "aide",
 		name: "AIDE高级设置版",
 		status: "stable",
-		link: "https://www.aidepro.top/",
+		link: "https://aidepro.netlify.app/",
 		icon: "https://previewengine.zoho.com.cn/image/WD/o9yvm0ce51d6b80f346969f2b9fd21529a330",
 		channel: 5,
 	},
