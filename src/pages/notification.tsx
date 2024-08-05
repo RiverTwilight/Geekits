2fe0ec94c1cc98b7445604326ce321d0a1705a85
rivertwilight <contact@rebe.wang>
2024-08-05 16:48:49 +0800

    fix: notification count

--- src/pages/notification.tsx
+++ src/pages/notification.tsx
@@ -7,90 +7,85 @@ import {
 	Link,
 	Divider,
 	Box,
-} from "@mui/material";
-import StyledMarkdown from "@/components/StyledMarkdown";
-import useNotifications from "@/utils/Hooks/useNotification";
-import { GetStaticProps } from "next";
-
-export const getStaticProps: GetStaticProps = ({ locale = "zh-CN" }) => {
+  } from "@mui/material";
+  import StyledMarkdown from "@/components/StyledMarkdown";
+  import useNotifications from "@/utils/Hooks/useNotification";
+  import { GetStaticProps } from "next";
+  import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+  
+  export const getStaticProps: GetStaticProps = ({ locale = "zh-CN" }) => {
 	const dic = require("../data/i18n.json");
-
+  
 	return {
-		props: {
-			currentPage: {
-				title: "更新公告",
-				path: "/notification",
-			},
-
-			locale,
-			dic: JSON.stringify(dic),
+	  props: {
+		currentPage: {
+		  title: "更新公告",
+		  path: "/notification",
 		},
+		locale,
+		dic: JSON.stringify(dic),
+	  },
 	};
-};
-
-const NotificationPage = () => {
+  };
+  
+  const NotificationPage = () => {
 	const [notifications, handleMarkAsRead] = useNotifications();
-
+  
 	return (
-		<Box
-			sx={{
-				width: { sm: "768px", xs: "100%" },
-				marginBottom: 4,
-				paddingX: { sm: 0, xs: 2 },
-			}}
-		>
-			{notifications
-				.reverse()
-				.filter((not) => !not.isRead)
-				.slice(0, 5)
-				.map((notification) => (
-					<Card
-						key={notification.id}
-						sx={{
-							marginBottom: 2,
-							borderRadius: "24px",
-							background: (theme) =>
-								theme.palette.background.paper,
-							boxShadow: "none",
-						}}
-					>
-						<CardContent>
-							<Typography variant="h5">
-								{notification.createDate.split("T")[0]}
-							</Typography>
-							<Divider />
-							<Box sx={{ paddingY: 2 }}>
-								<StyledMarkdown
-									content={notification.content}
-								/>
-							</Box>
-						</CardContent>
-						{!notification.isRead && (
-							<CardActions>
-								<Button
-									onClick={() =>
-										handleMarkAsRead(notification.id)
-									}
-								>
-									Mark as read
-								</Button>
-							</CardActions>
-						)}
-					</Card>
-				))}
-
-			<Box sx={{ display: "flex", justifyContent: "center" }}>
-				<Link
-					href="https://github.com/RiverTwilight/ygktool/issues/21"
-					target="_blank"
-					component={Button}
-					underline="none"
-				>
-					Check archived notification
-				</Link>
-			</Box>
+	  <Box
+		sx={{
+		  width: { sm: "768px", xs: "100%" },
+		  marginBottom: 4,
+		  paddingX: { sm: 0, xs: 2 },
+		}}
+	  >
+		{notifications
+		  .slice()
+		  .reverse()
+		  .filter((not) => !not.isRead)
+		  .slice(0, 5)
+		  .map((notification) => (
+			<Card
+			  key={notification.id}
+			  sx={{
+				marginBottom: 2,
+				borderRadius: "24px",
+				background: (theme) => theme.palette.background.paper,
+				boxShadow: "none",
+			  }}
+			>
+			  <CardContent>
+				<Typography variant="h5">
+				  {notification.createDate.split("T")[0]}
+				</Typography>
+				<Divider />
+				<Box sx={{ paddingY: 2 }}>
+				  <StyledMarkdown content={notification.content} />
+				</Box>
+			  </CardContent>
+			  {!notification.isRead && (
+				<CardActions>
+				  <Button onClick={() => handleMarkAsRead(notification.id)}>
+					Mark as read
+				  </Button>
+				</CardActions>
+			  )}
+			</Card>
+		  ))}
+  
+		<Box sx={{ display: "flex", justifyContent: "center" }}>
+		  <Link
+			href="https://github.com/RiverTwilight/ygktool/issues/21"
+			target="_blank"
+			component={Button}
+			underline="none"
+			endIcon={<OpenInNewIcon />}
+		  >
+			Check archived notification
+		  </Link>
 		</Box>
+	  </Box>
 	);
-};
-
-export default NotificationPage;
+  };
+  
+  export default NotificationPage;
--- src/utils/Hooks/useNotification.ts
+++ src/utils/Hooks/useNotification.ts
@@ -2,62 +2,59 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const API =
-	"https://api.github.com/repos/RiverTwilight/ygktool/issues/21/comments?sort=created";
+  "https://api.github.com/repos/RiverTwilight/ygktool/issues/21/comments?sort=created";
 
 interface INotification {
-	content: string;
-	id: number;
-	isRead: boolean;
-	createDate: string;
+  content: string;
+  id: number;
+  isRead: boolean;
+  createDate: string;
 }
 
 function useNotifications(): [INotification[], (id: number) => void] {
-	const [notifications, setNotifications] = useState<INotification[]>([]);
+  const [notifications, setNotifications] = useState<INotification[]>([]);
 
-	useEffect(() => {
-		const fetchNotifications = async () => {
-			const response = await axios.get(API);
-			const comments = response.data;
-			const readedRecords = JSON.parse(
-				localStorage.getItem("READED_NOTICES") || "[]"
-			);
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      const response = await axios.get(API);
+      const comments = response.data;
+      const readedRecords = JSON.parse(
+        localStorage.getItem("READED_NOTICES") || "[]"
+      );
 
-			const notifications = comments.map((comment) => ({
-				content: comment.body,
-				id: comment.id,
-				isRead: readedRecords.includes(comment.id),
-				createDate: comment.created_at, // Extracting the creation date
-			}));
+      const notifications = comments.map((comment) => ({
+        content: comment.body,
+        id: comment.id,
+        isRead: readedRecords.includes(comment.id),
+        createDate: comment.created_at,
+      }));
 
-			setNotifications(notifications);
-		};
+      setNotifications(notifications);
+    };
 
-		fetchNotifications();
-	}, []);
+    fetchNotifications();
+  }, []);
 
-	const setRead = (id: number) => {
-		const updatedNotifications = notifications.map((notification) => {
-			if (notification.id === id) {
-				notification.isRead = true;
-			}
-			return notification;
-		});
+  const setRead = (id: number) => {
+    setNotifications((prevNotifications) =>
+      prevNotifications.map((notification) =>
+        notification.id === id ? { ...notification, isRead: true } : notification
+      )
+    );
 
-		setNotifications(updatedNotifications);
+    const readedRecord = JSON.parse(
+      localStorage.getItem("READED_NOTICES") || "[]"
+    );
 
-		const readedRecord = JSON.parse(
-			localStorage.getItem("READED_NOTICES") || "[]"
-		);
+    if (!readedRecord.includes(id)) {
+      localStorage.setItem(
+        "READED_NOTICES",
+        JSON.stringify([...readedRecord, id])
+      );
+    }
+  };
 
-		if (!readedRecord.includes(id)) {
-			localStorage.setItem(
-				"READED_NOTICES",
-				JSON.stringify([...readedRecord, id])
-			);
-		}
-	};
-
-	return [notifications, setRead];
+  return [notifications, setRead];
 }
 
 export default useNotifications;
